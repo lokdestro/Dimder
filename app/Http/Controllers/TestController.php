b2bf24e2df0797bacd8d49245e2f07124f09a5a8
Дима Тишков <tishkovdima05@gmail.com>
2024-10-17 13:14:56 +0300

    loging2

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -10,10 +10,11 @@ class TestController extends Controller
 {
     public function index(){
         // user::create([
-        //     'name' => 'idiot',
-        //     'email' => 'tishkovmaksim11@gmail.com',
+        //     'name' => 'ali',
+        //     'email' => 'tishkovdima05@gmail.com',
         //     'password' => '1234'
         // ]);
+
         // Profile::create([
         //     'user_id' => 1, 
         //     'photo' => '', 
@@ -24,7 +25,21 @@ class TestController extends Controller
         //     'sex' => 'male', 
         //     'interests' => ''
         // ]);
+        
         // $button = Button::where('owner_id', $userId)->first();
+
+        // Profile::where('user_id', 1)->delete();
+
+
+        // $profile = Profile::where('user_id', 2)->first();
+
+        // if ($profile) {
+        //     $profile->bio = 'Обновленная информация о пользователе';
+        //     $profile->city = 'Брест';
+        //     $profile->save();
+        // }
+        
+
         $user = User::where('id', 1)
                         ->with(['profile'])
                         ->first();
